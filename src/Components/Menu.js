97ad22b960731f98dfe85796a29f5d6009aeacec
David Tuan <18521604@gm.uit.edu.vn>
2021-07-01 21:38:44 +0700

    Add InitialTransferMoney View

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -4,7 +4,7 @@ import settingIcon from "../assets/icons/setting.png";
 
 export function Menu(){
     return(
-        <div className="w-full flex flex-col items-center space-y-5 px-3 ">
+        <div className="w-full flex flex-col items-center space-y-5 px-3 sticky top-0">
             <div className="bg-group-color p-2 mt-4 rounded-2xl">
                 <img src={homeIcon} className="md:w-8 w-4" alt="homeicon"></img>
             </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,17 +3,24 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      fontFamily: {
+        title: ["Nunito"],
+        body: ['Segoe UI']
+      },
       width: {
         '1/18': '5.5555555%',
         '3/18': '16.6666667%',
         '14/18': '77.7777778%',
+        '11/20' : '55%',
         '4/5': '80%',
       },
       colors: {
         'button-color': '#25D366',
+        'hover-button-color': '#00a038',
         'group-color': '#075E54',
         'background-color': '#128C7E',
-        'secondtext-color': '#F4ECEC'
+        'secondtext-color': '#F4ECEC',
+        'icon-color': "#7E7A7A"
       },
     },
   },
